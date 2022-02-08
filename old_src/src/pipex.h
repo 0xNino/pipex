9c02514535342f7b7afff90ef7b4e7aa53cd555a
0xNino <86327707+0xNino@users.noreply.github.com>
2022-02-09 00:12:58 +0100

    move old src to temporary directory

---
+++ old_src/src/pipex.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: 0xNino <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/28 13:48:20 by 0xNino            #+#    #+#             */
+/*   Updated: 2022/02/01 23:05:39 by 0xNino           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include "../libft/libft.h"
+
+int		pipex(int argc, char **argv, char **envp);
+void	child_process(char **argv, char **envp, int *fd);
+void	parent_process(char **argv, char **envp, int *fd);
+void	error(char *error);
+void	execute(char *argv, char **envp);
+char	*get_path(char *cmd, char **envp);
+
+#endif
